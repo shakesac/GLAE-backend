363514a9658f6b1dfbd61aab69cc2c2024883db7
shakesac <miguel@drivewide.pt>
2021-05-22 18:04:37 +0100

    added DB_PORT to .env

--- app.js
+++ app.js
@@ -2,4 +2,12 @@ const express = require('express');
 const app = express()
 const api = process.env.API_URL;
 
+app.get('/', (req, res) =>{
+    res.status(200).send('A API está a correr!')
+})
+
+
+
 module.exports = app;
+
+
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const mysql  = require('mysql');
 //CRIAR LIGAÇÃO À BASE DE DADOS
 const dbconnection = mysql.createConnection({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_DATA
