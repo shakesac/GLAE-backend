1acadb89689e4d01135564519d95668df04e3335
shakesac <miguel@drivewide.pt>
2021-07-15 22:59:02 +0100

    Vue-Router added

---
+++ frontend/1240armazem/src/components/Buttons.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <b-button @click="onClick()" :variant="variant">{{ text }}</b-button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    props: {
+        text: String,
+        variant: String,
+    },
+    methods: {
+        onClick() {
+            console.log('click')
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
