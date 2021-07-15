e625fef9b1c49b41ed0e7172d92d1a6050ba37bb
shakesac <miguel@drivewide.pt>
2021-07-15 16:13:04 +0100

    bootstrap-vue installed and imported

---
+++ frontend/1240/src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import App from './App.vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
+createApp(App).mount('#app')
