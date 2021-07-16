8188f95b7eedcd218b80c8e0dad9a4d5953099b4
shakesac <miguel@drivewide.pt>
2021-07-16 11:35:49 +0100

    updated d-flex

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,50 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="center-container d-flex text-center text-white bg-dark">
+    <div class="cover-container w-100 p-3 mx-auto justify-content-center d-flex flex-column">
+    <main class="px-3 d-flex flex-column">
+      <h1>Cover your page.</h1>
+      <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+      <p class="lead">
+        <a href="#" class="btn btn-lg btn-secondary fw-bold border-white bg-white">Learn more</a>
+      </p>
+    </main>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
+
+<style>
+  .center-container {
+    height: calc(100vh - 56px);
+  }
+  .bd-placeholder-img {
+    font-size: 1.125rem;
+    text-anchor: middle;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    user-select: none;
+  }
+  @media (min-width: 768px) {
+    .bd-placeholder-img-lg {
+      font-size: 3.5rem;
+    }
+  }
+  .cover-container {
+    max-width: 42em;
+  }
+  .btn-secondary,
+  .btn-secondary:hover,
+  .btn-secondary:focus {
+    color: #333;
+    text-shadow: none; /* Prevent inheritance from `body` */
+  }
+</style>
--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,32 +1,11 @@
 <template>
-<main>
-    <div class="container" id="loginContainer">
-        <div class="row">
-            <div class="col-md-4 offset-md-4">
-                <div class="login-form bg-light mt-4 p-4">
-                    <form action="" method="" class="row g-3">
-                        <h4>Bem-vindo</h4>
-                        <div class="col-12">
-                            <label>Endereço de email</label>
-                            <input type="text" name="username" class="form-control" placeholder="Username">
-                        </div>
-                        <div class="col-12">
-                            <label>Palavra-passe</label>
-                            <input type="password" name="password" class="form-control" placeholder="Password">
-                        </div>
-                        <div class="col-12">
-                            <button type="submit" class="btn btn-dark float-end">Login</button>
-                        </div>
-                    </form>
-                    <hr class="mt-4">
-                    <div class="col-12">
-                        <p class="text-center mb-0">Ainda não tens conta? <a href="#">Regista-te</a></p>
-                    </div>
-                </div>
-            </div>
-        </div>
+  <div class="center-container d-flex text-center text-white bg-dark">
+    <div class="cover-container w-100 p-3 mx-auto">
+    <main class="px-3 d-flex flex-column justify-content-center">
+        
+    </main>
     </div>
-</main>
+  </div>
 </template>
 
 <script>
@@ -36,7 +15,4 @@ export default {
 </script>
 
 <style>
-main {
-    height: calc(100vh - 116px)
-}
 </style>
