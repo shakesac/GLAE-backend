d81a2f10686001aefa04510de4e72d1e3ddc0499
shakesac <miguel@drivewide.pt>
2021-07-18 13:07:30 +0100

    removed html tags

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <Header />
-  <div id="bg-wrapper" class="container-fluid d-flex align-items-center">
-    <div id="center-wrapper" class="container-sm d-flex text-center">
-      <router-view/>
-    </div>
-  </div>
+  <router-view/>
 </template>
 <script>
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -28,22 +24,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   /*color: #2c3e50;*/
   background: #212529;
-}
-
-  #bg-wrapper {
-  background: #212529 url('./assets/background.jpg');
-  background-repeat: no-repeat;
-  background-attachment: fixed;
-  background-position: center;
-  background-size: cover;
-  min-height: calc(100vh - 56px);
-  padding-top: 3vh;
-  padding-bottom: 3vh;
-}
-
-#center-wrapper {
-  background-color: #f0f3f6;
-  border-radius: 40px;
-  padding: 75px 40px;
 }
 </style>
