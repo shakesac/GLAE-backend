70ce72bbb7a9048f61488815a0bc9fb7459c1ecb
shakesac <miguel@drivewide.pt>
2021-07-18 21:14:08 +0100

    x-access-token headers added

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+
 const headers = {
     Accept: "application/json",
     ContentType: 'application/x-www-form-urlencoded',
@@ -6,9 +7,23 @@ const headers = {
 
 const api = axios.create({
     baseURL: 'http://127.0.0.1:5000/api/v1',
-    withCredentials: true,
-    credentials: 'include',
+    /*withCredentials: true,
+    credentials: 'include',*/
     headers
 });
+
+
+//Intersepta os requests e adiciona o header x-access-token caso o utilizador esteja logado.
+api.interceptors.request.use((config) => {
+    let user = JSON.parse(localStorage.getItem('user'))
+    if (user && user.token) {
+        config.headers['x-access-token'] = user.token
+    }
+      return config
+    }, 
+    (error) => {
+      return Promise.reject(error)
+    }
+)
 
 export default api;
