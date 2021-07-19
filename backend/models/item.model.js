c05824d48b9b524e47005c30e2912c37e9513844
shakesac <miguel@drivewide.pt>
2021-07-19 12:55:23 +0100

    added description and validation

--- backend/models/item.model.js
+++ backend/models/item.model.js
@@ -9,8 +9,11 @@ const Item = sequelize.define('item', {
         primaryKey: true
     },
     name: {
-        type: Sequelize.STRING,
+        type: Sequelize.STRING(200),
         allowNull: false,
+    },
+    description: {
+        type:Sequelize.TEXT
     },
     purchasedAt: {
         type: Sequelize.DATEONLY,
@@ -18,11 +21,19 @@ const Item = sequelize.define('item', {
     createdBy: {
         type: Sequelize.INTEGER,
         allowNull: false,
-    }
+    },
+    
 },
 {
     timestamps: true,
     updatedAt: false,
+    validate: {
+        checkComment() {
+            if (this.description && this.description.length > 1000) {
+                throw new Error('A descrição não pode ter mais de 1000 caracteres.')
+            }
+        }
+    }
 })
 
 module.exports = Item
