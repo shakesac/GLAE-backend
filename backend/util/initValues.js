c92840352e3709aea397305aec22e9e9b5ac88db
shakesac <miguel@drivewide.pt>
2021-07-19 12:56:57 +0100

    added more objects

--- backend/util/initValues.js
+++ backend/util/initValues.js
@@ -4,6 +4,8 @@ const Section = require("../models/section.model");
 const Subsection = require('../models/subsection.model')
 const Role = require("../models/user-role.model");
 const User = require("../models/user.model");
+const ItemsCategory = require('../models/item-cat.model')
+const ItemType = require('../models/item-type.model')
 
 exports.create = async () => {
     const roles = await Role.bulkCreate([
@@ -16,7 +18,7 @@ exports.create = async () => {
         { id: 2, section: 'Expedição'},
         { id: 3, section: 'Comunidade'},
         { id: 4, section: 'Clã'}
-    ])
+    ]).catch(err =>{ console.log(err.message) })
     const subsections = await Subsection.bulkCreate([
         { id: 1, subsection: 'Branco', sectionId: 1},
         { id: 2, subsection: 'Cinzento', sectionId: 1},
@@ -32,6 +34,33 @@ exports.create = async () => {
         { id: 2, subsection: 'Camões', sectionId: 3},
         { id: 3, subsection: 'Padeira de Aljubarrota', sectionId: 3},
         { id: 1, subsection: 'Vasco da Gama', sectionId: 4},
+    ]).catch(err =>{ console.log(err.message) })
+    const ItemsCategories = await ItemsCategory.bulkCreate([
+        { id: 1, category: 'Sede'},
+        { id: 2, category: 'Abrigo'},
+        { id: 3, category: 'Cozinha'},
+        { id: 4, category: 'Ferramentas'},
+        { id: 5, category: 'Energizados'},
+        { id: 6, category: 'Socorrismo'},
+        { id: 7, category: 'Desportivos'},
+        { id: 8, category: 'Diversos'},
+        { id: 9, category: 'Jogos'}
+    ])
+    const ItemsCategories = await ItemsCategory.bulkCreate([
+        { id: 01, subsection: 'Dossier', itemCategoryId: 1},
+        { id: 2, subsection: 'Cinzento', itemCategoryId: 1},
+        { id: 3, subsection: 'Castanho', itemCategoryId: 1},
+        { id: 4, subsection: 'Preto', itemCategoryId: 1},
+        { id: 5, subsection: 'Ruivo', itemCategoryId: 1},
+        { id: 1, subsection: 'Pantera', itemCategoryId: 2},
+        { id: 2, subsection: 'Mocho', itemCategoryId: 2},
+        { id: 3, subsection: 'Falcão', itemCategoryId: 2},
+        { id: 4, subsection: 'Leão', itemCategoryId: 2},
+        { id: 5, subsection: 'Touro', itemCategoryId: 2},
+        { id: 1, subsection: 'B.P.', itemCategoryId: 3},
+        { id: 2, subsection: 'Camões', itemCategoryId: 3},
+        { id: 3, subsection: 'Padeira de Aljubarrota', itemCategoryId: 3},
+        { id: 1, subsection: 'Vasco da Gama', itemCategoryId: 4},
     ])
 
     //DEV - DADOS DE TESTE
@@ -44,5 +73,5 @@ exports.create = async () => {
             phoneNumber: '+351910000000', 
             password: hashSync('forte', parseInt(process.env.BCRYPT_SALT))
         }
-    ])
+    ]).catch(err =>{ console.log(err.message) })
 }
