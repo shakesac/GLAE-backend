0488be653557c43b9f5988af17d8839a986e32cd
shakesac <miguel@drivewide.pt>
2021-07-19 12:55:06 +0100

    added description and validation

--- backend/models/item-type.model.js
+++ backend/models/item-type.model.js
@@ -3,19 +3,36 @@ const sequelize = require('../util/db')
 
 const ItemType = sequelize.define('item_type', {
     id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
+        type: Sequelize.INTEGER(2),
         allowNull: false,
         primaryKey: true
     },
-    name: {
-        type: Sequelize.STRING,
+    type: {
+        type: Sequelize.STRING(200),
         allowNull: false,
-    }
-    
+    },
+    itemCategoryId: {
+        type: Sequelize.INTEGER(2),
+        allowNull: false,
+        primaryKey: true
+    }    
 },
 {
-    timestamps: false
+    timestamps: true,
+    updatedAt: false,
+    validate: {
+        checkId() {
+            if (this.id < 0 || this.id > 99) {
+                throw new Error('O código deverá ser um valor entre 0 e 99.')
+            }
+        },
+        checkComment() {
+            if (this.comment.length !== 2) {
+                throw new Error('O código deverá ser composto por 2 algarismos. (Ex.: 01, 34, 68)')
+            }
+        }
+    }
+
 })
 
 module.exports = ItemType
