a6f4de03d2a671500021150d5df135b971a4b74c
shakesac <miguel@drivewide.pt>
2021-07-19 15:27:24 +0100

    added hasConstraints()

--- backend/util/contr.helpers.js
+++ backend/util/contr.helpers.js
@@ -1,4 +1,14 @@
-export async function checkIfExists(id, obj) {
+// Verifica se o id do objecto enviado existe na base de dados
+exports.checkIfExists = async (id, obj) => {
     const result = await obj.findByPk(id)
     return result ? true : false
+}
+
+// Verifica se objecto1 tem restrições na tabela do objecto 2
+exports.hasConstraints = async (obj, idObj, fieldName) => {
+    const result = await obj.findAndCountAll({
+        where: { [fieldName]: idObj  }
+    })
+    console.log('Resultado', result)
+    return result.count > 0 ? true : false
 }
