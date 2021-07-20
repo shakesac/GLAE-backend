1fc7967fd9894d3ae045defa890221ccc5d3cc6c
shakesac <miguel@drivewide.pt>
2021-07-20 13:32:27 +0100

    workaround for .env variable not working

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
-const apiUrl = process.env.API_URL
-
+let apiUrl = process.env.API_URL
+apiUrl = 'http://127.0.0.1:5000/api/v1'
+console.log(apiUrl)
 const headers = {
     Accept: "application/json",
     ContentType: 'application/x-www-form-urlencoded',
