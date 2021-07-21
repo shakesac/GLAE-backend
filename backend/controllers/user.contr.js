94ff6152214cee0431d5b7f686085a5fe6955294
shakesac <miguel@drivewide.pt>
2021-07-21 03:09:19 +0100

    minor fix

--- backend/controllers/user.contr.js
+++ backend/controllers/user.contr.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcrypt')
 const bcryptSalt = parseInt(process.env.BCRYPT_SALT)
 const helper = require('../util/contr.helpers')
 const User = require('../models/user.model')
+const Item = require('../models/item.model')
 const Subsection = require('../models/subsection.model')
 const { options } = require('../routes/auth.route')
 
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -2,9 +2,11 @@ const express = require('express')
 const router = express.Router()
 const {verify} = require('../controllers/auth.contr')
 const userContr = require('../controllers/user.contr')
+const User = require('../models/user.model')
 
 router.put('/update/:id', userContr.update)
 router.get('/all', userContr.getAll)
 router.get('/:id', userContr.get)
+router.delete('/:id', userContr.delete)
 
 module.exports = router
