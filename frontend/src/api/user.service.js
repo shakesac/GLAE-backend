27b6cb0de10411308d45c40c74d648b0c7a66f7e
shakesac <miguel@drivewide.pt>
2021-07-21 02:15:55 +0100

    created user service

---
+++ frontend/src/api/user.service.js
@@ -0,0 +1,23 @@
+import api from './api'
+import { handleResponses } from './err.service'
+
+export const authService = {
+    async login(payload) {
+      const res = await api.post('/login', payload)
+      if (res.status == 200) {
+        const token = res.data.data.token
+        const profile = res.data.data.user
+        return { token, profile }
+      } else {
+        throw Error(handleResponses(res))
+      }
+    },
+    async register(payload) {
+      const res = await api.post('/register', payload)
+      if (res.ok) {
+        return res
+      } else {
+        throw new Error(res)
+      }
+    },
+  }
