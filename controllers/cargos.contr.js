44bcff584e7825c1cf71625d9fdbb23f87e51969
shakesac <miguel@drivewide.pt>
2021-09-15 00:20:11 +0100

    rewrited

--- controllers/cargos.contr.js
+++ controllers/cargos.contr.js
@@ -1,32 +1,83 @@
+const AppError = require('../util/appError')
+const { Op } = require("sequelize")
 const helper = require('../util/contr.helpers')
 const Cargo = require('../models/cargo.model')
 
-exports.new = async (req, res) => {
-    const { cargo } = req.body
-    const newCargo = new Cargo({
-        cargo
-    })
-    await helper.create(res, Cargo)
+exports.new = async (req, res, next) => {
+    try {
+        const { cargo } = req.body
+        await Cargo.create({
+            cargo
+        })
+        res.status(201).json({
+            status: 'success',
+            message: 'O cargo foi criado com sucesso.'
+        })
+    } catch(err) {
+        console.log(err)
+        return next(new AppError(err.toString(), 500, 'error'))
+    }
 }
 
-exports.update = async (req, res) => {
-    await helper.checkIfByPkAndUpdate(res, Cargo, req.params.id, req.body)
+exports.update = async (req, res, next) => {
+    try {
+        const { cargo } = req.body
+        const cargoId = req.params.id
+        const result = await Cargo.update({ cargo }, { where: { id: cargoId }})
+        if (result[0] > 0) {
+            return res.status(200).json({
+                status: 'success',
+                message: 'O cargo foi alterado.'
+            })
+        }
+        else return next(new AppError('Não foram efetuadas alterações.', 400, 'failed'))
+    } catch(err) {
+        console.log(err)
+        return next(new AppError(err.toString(), 500, 'error'))
+    }
 }
 
-exports.get = async (req, res) => {
-    helper.checkIfByPkAndGet(res, Cargo, req.params.id)
+exports.get = async (req, res, next) => {
+    try {
+        const cargo = await Cargo.findByPk(req.params.id)
+        if (!cargo) return next(new AppError('O cargo indicado não existe.', 404, 'not found'))
+        else return res.status(200).json({
+            status: "success",
+            data: cargo
+        })
+    } catch(err) {
+        console.log(err)
+        return next(new AppError(err.toString(), 500, 'error'))
+    }
 }
 
-exports.getAll = async (req, res) => {
-    await helper.checkIfAndGetAll(res, Cargo)
+exports.getAll = async (req, res, next) => {
+    try {
+        const cargos = await Cargo.findAll()
+        if (!cargos) return next(new AppError('Não existem cargos.', 404, 'not found'))
+        else return res.status(200).json({
+            status: "success",
+            data: cargos
+        })
+    } catch(err) {
+        console.log(err)
+        return next(new AppError(err.toString(), 500, 'error'))
+    }
 }
 
-exports.delete = async (req, res) => {
-    await helper.delete(
-        res,
-        Cargo,
-        req.params.id,
-        Uers,
-        'cargoId'
-        )
+exports.delete = async (req, res, next) => {
+    try {
+        const cargo = await Cargo.findByPk(req.params.id)
+        if (!cargo) return next(new AppError('O cargo indicado não existe.', 404, 'not found'))
+        else {
+            cargo.destroy()
+            return res.status(200).json({
+                status: 'success',
+                message: 'O cargo foi eliminado.'
+            })
+        }
+    } catch(err) {
+        console.log(err)
+        return next(new AppError(err.toString(), 500, 'error'))
+    }
 }
--- routes/cargos.routes.js
+++ routes/cargos.routes.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const cargoContr = require('../controllers/cargos.contr')
-const {verify} = require('../controllers/auth.contr')
+const {verify, isAdmin} = require('../controllers/auth.contr')
 
-router.post('/new', cargoContr.new)
-router.put('/update/:id', cargoContr.update)
-router.delete('/delete/:id', cargoContr.delete)
+router.post('/new', verify, isAdmin, cargoContr.new)
+router.put('/:id', cargoContr.update)
+router.delete('/:id', cargoContr.delete)
 router.get('/all', cargoContr.getAll)
 router.get('/:id', cargoContr.get)
 
